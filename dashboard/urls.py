acbd96c08db61660a594ca0ff9a6094614a3d0e2
mozsocia <mozsocia@gmail.com>
2023-07-08 11:16:15 +0600

    hhh to admin

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('hhh', dashboard, name='dashboard'),
+    path('admin/', dashboard, name='dashboard'),
 
     # slider
     # path('rrr', views.slider_list, name='list'),
