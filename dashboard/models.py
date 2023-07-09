2d7fbd5f228a529ad349e9a3a6f4cc8d6979bd27
Naymui <anirchanda21@gmail.com>
2023-07-09 15:52:58 +0600

    car_pricing

--- dashboard/models.py
+++ dashboard/models.py
@@ -77,7 +77,11 @@ class Pricing(models.Model):
     car_name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='car_images/')
-    price = models.DecimalField(max_digits=8, decimal_places=2)  
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    Car_Seats = models.DecimalField(max_digits=8, decimal_places=2,blank=True,null=True)
+    car_type = models.CharField(max_length=100,blank=True,null=True)
+
+
 
     menu_choices = (
         ('hourly', 'Hourly Car Rental'),
